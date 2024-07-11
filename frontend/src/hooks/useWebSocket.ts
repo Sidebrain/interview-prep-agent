9e19ee4c4a56068a5e301db3b486423b3e6bd20a
Anudeep Yegireddi <anudeep.yegireddi@gmail.com>
2024-07-11 19:50:02 +0530

    feat: added a websocket hook
    Note:
    - Built a much more complicated version, but this is the simplest
      version
    - All it does it set up a websocket, handle unmount and console log when
      basic events are received
    - User available functions are to send a ping, and close a connection.
      No other options available currently
    - No states, only a single ref is doing the heavy lifting
    - Had multiple websocket connections being set up simlulatneously fixed
      that

---
+++ frontend/src/hooks/useWebSocket.ts
@@ -0,0 +1,62 @@
+import { useCallback, useEffect, useRef } from "react";
+
+const useWebSocket = (url: string) => {
+  const websocketRef = useRef<WebSocket | null>(null);
+
+  const connect = useCallback(() => {
+    /*
+    Set up a websocket connection and pass it into the ref.
+    */
+    if (websocketRef.current) {
+      return;
+    }
+    const ws = new WebSocket(url);
+    websocketRef.current = ws;
+
+    websocketRef.current.onopen = () => {
+      console.log("On open triggered");
+    };
+
+    websocketRef.current.onmessage = () => {
+      console.log("message received.");
+    };
+
+    websocketRef.current.onclose = () => {
+      console.log("Websocket closed");
+    };
+  }, [url]);
+
+  const sendMessage = useCallback(() => {
+    if (
+      websocketRef.current &&
+      websocketRef.current.readyState === WebSocket.OPEN
+    ) {
+      websocketRef.current.send("ping");
+    }
+  }, []);
+
+  const closeSocket = useCallback(() => {
+    if (websocketRef.current) {
+      websocketRef.current.close(1000, "Client closing connection");
+    }
+  }, []);
+
+  useEffect(() => {
+    connect();
+    return () => {
+      if (websocketRef.current && websocketRef.current.readyState === 1) {
+        websocketRef.current.close(1000, "component unmounting");
+      } else {
+        console.log("No websocket connection to close.");
+      }
+      console.log("useEffect cleanup done.");
+    };
+  }, [connect]);
+
+  return {
+    sendMessage,
+    closeSocket,
+  };
+};
+
+export default useWebSocket;
